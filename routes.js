9fe39e9b595291b7c229f396d760daa8d9473b4e
Brian J Brennan <brianloveswords@gmail.com>
2011-03-10 02:05:09 -0500

    Adjusted order of routes.

--- routes.js
+++ routes.js
@@ -7,19 +7,6 @@ router.map(function(){
   this.root.bind(function(req, res) {
     res.send({version: '0.1.0'});
   });
-  /* issuer/ */
-  this.post(/issuer\/?/).bind(function(req, res, data) {
-    res.send({
-      route: "POST issuer/",
-      data: data
-    });
-  });
-  this.put(/issuer\/(.*)/).bind(function(req, res, id, data) {
-    res.send({
-      route: "PUT issuer/" + id,
-      data: data
-    });
-  });
 
   /* issuer/badge */
   this.post(/issuer\/badge\/?/).bind(function(req, res, data) {
@@ -28,14 +15,28 @@ router.map(function(){
       data: data
     });
   });
-  this.put(/issuer\/badge\/(.*)/).bind(function(req, res, id, data) {
+  this.put(/issuer\/badge\/(\d+)/).bind(function(req, res, id, data) {
     res.send({
       route: "PUT issuer/badge" + id,
       data: data
     });
   });
-  this.del(/issuer\/badge\/(.*)/).bind(function(req, res, id) {
+  this.del(/issuer\/badge\/(\d+)/).bind(function(req, res, id) {
     res.send("DELETE issuer/badge/" + id);
+  });
+  
+  /* issuer/ */
+  this.post(/issuer\/?/).bind(function(req, res, data) {
+    res.send({
+      route: "POST issuer/",
+      data: data
+    });
+  });
+  this.put(/issuer\/(\d+)/).bind(function(req, res, id, data) {
+    res.send({
+      route: "PUT issuer/" + id,
+      data: data
+    });
   });
 })
 
